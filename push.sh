dddcc34eba601cc523ef9691c53ecac23a2ba2d1
andixmb <andixmbacen@gmail.com>
2024-07-02 16:20:47 +0000

    Commit on 2024-07-02 16:20:47

---
+++ push.sh
@@ -0,0 +1,11 @@
+# Set current date as commit message
+commit_message="Commit on $(date +"%Y-%m-%d %H:%M:%S")"
+
+# Stage all changes
+git add .
+
+# Commit changes with the current date as message
+git commit -m "$commit_message"
+
+# Push to the remote repository using GitHub CLI
+gh repo sync origin main
